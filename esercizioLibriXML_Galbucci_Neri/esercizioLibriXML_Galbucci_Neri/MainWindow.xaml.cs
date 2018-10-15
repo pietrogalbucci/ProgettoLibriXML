9e47ee8ab89753d070e2644ed70d682150c0de0b
pietrogalbucci <pietro.galbucci@gmail.com>
2018-10-15 10:41:47 +0200

    test

---
+++ esercizioLibriXML_Galbucci_Neri/esercizioLibriXML_Galbucci_Neri/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace esercizioLibriXML_Galbucci_Neri
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Crea_Click(object sender, RoutedEventArgs e)
+        {
+            XDocument xmlDocument = XDocument.Load(@"Z:\Tpi\esercizioLibriXML\esercizioLibriXML_Galbucci_Neri\esercizioLibriXML_Galbucci_Neri\libri.xml");
+        }
+    }
+}
